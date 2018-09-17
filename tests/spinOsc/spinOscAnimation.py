corrected noise variance to 2 gamma^2 D, and finished spinOscLangevin class in spinOscSimulation.py

tests/spinOsc/spinOscAnimation.py
@@ -10,7 +10,7 @@ F = -k*r + alpha * curl(z,r)
 
 The k term represents a harmonic potential
 the alpha term represents a rotational, non-conservative force for the potential
-xi is Gaussian white noise with strength sqrt(2*gamma*D), D is the diffusion constant
+xi is Gaussian white noise with strength sqrt(2*gamma^2*D), D is the diffusion constant
 which is given by the Einstein-relation D = kB*T/gamma
 '''
 
@@ -20,14 +20,14 @@ import matplotlib.pyplot as plt
 # import matplotlib as mpl
 import matplotlib.animation as animation
 
-gamma = 1.7e-8  # drag on particle in kg / s
+gamma = 2e-8 # drag on particle in kg / s
 kT = 4e-9  # energy in kg * um^2 / s^2
 # kT = 4e-21  # energy in Joules at room temp
 D = kT / gamma
 dt = 1e-3  # time step
 
-k = 0  # spring constant of harmonic potential
-alpha = 0  # strength of rotational force
+k = 2*gamma  # spring constant of harmonic potential
+alpha = 1*gamma  # strength of rotational force
 zhat = np.array([0, 0, 1])
 nframes = int(2e3)
 
@@ -39,19 +39,19 @@ def update(p, r):
     q = (p + 1)
     springForce = - k * r[p]
     rotationalForce = alpha * np.cross(zhat, np.append(r[p], 0))[:2]
-    noise = np.sqrt(2 * gamma * D / dt) * np.random.randn(2)
+    noise = np.sqrt(2 * gamma**2 * D / dt) * np.random.randn(2)
 
-    r[q] = r[p] + (springForce + rotationalForce + noise) * dt
+    r[q] = r[p] + (springForce + rotationalForce + noise) * dt / gamma
     return r
 
 
 # Initialize random data. Row is time point, columns are x and y
-xmax = 1e-3
-ymax = 1e-3
+xmax = 1
+ymax = 1
 r = (np.random.rand(nframes, 2) - 0.5) * [xmax, ymax]
 
 fig, ax = plt.subplots()
-ax.set_title(r'$\alpha$ = {a}, $k$ = {k}'.format(a=alpha, k=k))
+ax.set_title(r'$\tau_\alpha$ = {a}, $\tau_k$ = {k}'.format(a=gamma / alpha, k=gamma / k))
 ax.set_xlim(-xmax, xmax)
 ax.set_ylim(-ymax, ymax)
 ax.set_aspect('equal')

tests/spinOsc/spinOscSimulation.py
@@ -10,27 +10,14 @@ F = -k*r + alpha * curl(z,r)
 
 The k term represents a harmonic potential
 the alpha term represents a rotational, non-conservative force for the potential
-xi is Gaussian white noise with strength sqrt(2*gamma*D), D is the diffusion constant
+xi is Gaussian white noise with strength sqrt(2*gamma^2*D), D is the diffusion constant
 which is given by the Einstein-relation D = kB*T/gamma
 '''
 
-
 import numpy as np
-import matplotlib.pyplot as plt
-# import matplotlib as mpl
 
 
-# Parameters
-gamma = 1  # drag on particle
-kT = 0.1  # energy in g * um^2 / s^2
-D = kT / gamma
-dt = 1e-2  # time step
-k = 1  # spring constant of harmonic potential
-alpha = 1.5  # strength of rotational force
-nframes = int(2e3)
-
-
-class Langevin():
+class spinOscLangevin():
     '''
     class for solving the 2D overdamped langevin equation
 
@@ -42,20 +29,20 @@ class Langevin():
         alpha is strength of rotational force
     xi is zero mean Gaussian white noise
         <xi> = 0
-        <xi_i (t) xi_j (t')> = 2*D*gamma * delta_ij * delta(t - t')
+        <xi_i (t) xi_j (t')> = 2*D*gamma^2 * delta_ij * delta(t - t')
             D is diffusion constant, D = kB*T/gamma
 
     '''
 
-    def __init__(self, dt, tfinal=10, alpha=1, k=1,
-                 kT=4e-9, gamma=2e-8, r0=np.zeros(2)):
+    def __init__(self, dt, tfinal=10, kT=4e-9, gamma=2e-8, r0=np.zeros(2)):
+
         self.dt = dt  # time step in seconds
         self.tfinal = tfinal  # final time in seconds
-        self.nsteps = self.tfinal / self.dt  # total number of steps
-        self.alpha = alpha  # strength of rotational force in Newtons /
-        self.k = k  # strength of harmonic potential in
+        self.nsteps = int(self.tfinal / self.dt)  # total number of steps
+        self.alpha = alpha  # strength of rotational force in uN
+        self.k = k  # strength of harmonic potential in kg / s^2
         self.gamma = gamma  # drag on particle in kg / s
-        self.kT = kT  # in kg * microns^2 / seconds^2
+        self.kT = kT  # in kg * um^2 / s^2
 
         # derived quantities
         self.D = self.kT / self.gamma
@@ -69,27 +56,32 @@ class Langevin():
                       self.k, self.kT, self.gamma,
                       self.r0)
 
-    def springForce(self, r):
+    def springForce(self, r, k):
         '''
         harmonic force
         '''
-        return -self.k * r
+        return -k * r
 
-    def rotationalForce(self, r):
+    def rotationalForce(self, r, alpha):
         '''
         rotational force
         '''
         zhat = np.array([0, 0, 1])
-        return self.alpha * np.cross(zhat, np.append(r, 0))[:2]
+        return alpha * np.cross(zhat, np.append(r, 0))[:2]
 
     def noise(self):
         '''
         Gaussian white noise
         '''
-        return np.sqrt(2 * self.gamma * self.D / self.dt) * np.random.randn(2)
+        return np.sqrt(2 * self.D * self.gamma**2 / self.dt) * np.random.randn(2)
 
-    def runSimulation(self):
+    def runSimulation(self, k, alpha):
         for index, time in enumerate(self.t[1:]):
             pos_old = self.pos[index]
-            pos_new = pos_old + (self.springForce(pos_old) + self.rotationalForce(pos_old) + self.noise()) * self.dt
+            pos_new = pos_old + (self.springForce(pos_old, k) + self.rotationalForce(pos_old, alpha) + self.noise()) * self.dt / self.gamma
             self.pos[index + 1] = pos_new
+
+
+# def run(dt=1e-3, tfinal=1, alpha=1, k=1, kT=4e-9, gamma=2e-8, r0=np.random.rand(2)):
+#     t = 0
+#     while t<tfinal