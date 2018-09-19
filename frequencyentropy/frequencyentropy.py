1ab6a04041f9957cbab5588b6aa180d7d0bf2529
Danny Seara <dsseara@gmail.com>
2018-09-19 18:53:10 -0400

    created wrapper around scipy.signal.correlate to give mean rather than sum to calculate correlation functions

--- frequencyentropy/frequencyentropy.py
+++ frequencyentropy/frequencyentropy.py
@@ -1,20 +1,73 @@
 import numpy as np
+import scipy.signal as signal
+import warnings
 
 
-def corrMatrix(data):
+def corrMatrix(data, sample_rate=1, return_fft=False):
     '''
-    Takes the data matrix and returns a correlation matrix for every lag time
+    Takes time series data of multiple variables and returns a correlation matrix
+    for every lag time
 
     Parameters
     ----------
-    data : 2d numpy array
-        data contains time series data of multiple variables. The first dimension
-        contains each variable, and the second dimension contains the time series
-        for that variable. e.g. data[0] returns time series for first variable.
+    data : 2D array
+        data is an NxM array that gives length M time series data of N variables.
+        e.g. data[0] returns time series for first variable.
+    sample_rate : scalar
+        sample rate of data in seconds. Default = 1
+    return_fft : bool (optional)
+        boolean asking whether to return the temporal fourier transform of the
+        correlation matrix
+
+    Returns
+    -------
+    c : 3D array
+        an NxNx(2M-1) matrix that gives the NxN correlation matrix for
     '''
 
     data = np.asarray(data)
+    if data.shape[0] > data.shape[1]:
+        warnings.warn('Number of rows (variables) > number of columns (time points). '
+                      'Make sure data has variables as rows.')
 
-    nrows, ncols = data.shape
-    for x in range(1, 10):
-        pass
+    nvars, npts = data.shape
+    c = np.zeros((nvars, nvars, npts))
+
+    # get all pairs of indices
+    idx_pairs = np.array(np.meshgrid(np.arange(nvars), np.arange(nvars))).T.reshape(-1, 2)
+
+    for idx in idx_pairs:
+        c[idx[0], idx[1], :] = _correlate_mean(data[idx[0]], data[idx[1]], return_fft)
+
+    return c
+
+
+def _correlate_mean(x1, x2, mode='full', method='auto', return_fft=False):
+    '''
+    Calculate cross-correlation between two time series using fourier transform
+    Wrapper around scipy.signal.correlate function that takes a mean rather than
+    just summing up the signal
+
+    Parameters
+    ----------
+    x, y : 1D array
+        data to find cross-correlation between
+    return_fft : bool (optional)
+        boolean asking whether to return the temporal fourier transform of the
+        correlation matrix
+
+    Returns
+    -------
+    xcorr : 1D array
+        cross correlation between
+    '''
+
+    N = max(len(x1), len(x2))
+    n = N * np.ones(N) - np.arange(N)
+    n = np.concatenate((np.flipud(n)[:-1], n))
+    xcorr = signal.correlate(x1, x2, mode, method) / n
+
+    if return_fft:
+        return np.fft.fftshift(np.fft.fft(xcorr))
+    else:
+        return xcorr
