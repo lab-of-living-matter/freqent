f859a5ad8788d3d3d59b143b39e55a1866f304ee
Danny Seara <dsseara@gmail.com>
2019-01-30 17:42:31 -0500

    created plotting script for output of make_parallelSims.py

---
+++ freqent/tests/bz/make_brusselatorEPRPlot.py
@@ -0,0 +1,25 @@
+import os
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import freqent.freqent as fe
+
+folder = '/mnt/llmStorage203/Danny/brusselatorSims/reactionsOnly/190128'
+
+alpha = np.asarray([float(x.split('alpha')[-1].split('_')[0]) for x in glob(os.path.join(folder, '*/'))])
+
+epr = np.zeros(len(alpha))
+epr_spectral = np.zeros(len(alpha))
+for fInd, f in enumerate(glob(os.path.join(folder, '*/'))):
+    with open(os.path.join(f, 'data.pickle'), 'rb') as d:
+        data = pickle.load(d)
+        epr[fInd] = data['epr']
+        epr_spectral[fInd] = data['epr_spectral']
+
+fig, ax = plt.subplots()
+ax.loglog(alpha, epr, '.')
+ax.loglog(alpha, epr_spectral, '.')
+ax.set(xlabel=r'$\alpha$', ylabel=r'$\dot{\Sigma}$')
+
+plt.show()
