31823b92ce5afea77d9caa9e837573ace4f02d48
Danny Seara <dsseara@gmail.com>
2019-09-10 16:51:44 -0400

    changed brussfield/make_eprPlot.py and brusselator/calculate_epr.py to do things with each trajectory individually rather than averaging all trajectories together

--- freqent/tests/brusselator/calculate_epr.py
+++ freqent/tests/brusselator/calculate_epr.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from glob import glob
 import numpy as np
 # import matplotlib.pyplot as plt
@@ -11,40 +12,50 @@ def calc_epr_spectral(file):
     '''
     function to pass to multiprocessing pool to run parallel simulations
     '''
-    print('Reading {f}'.format(f=file))
-    with h5py.File(file, 'r') as d:
+    print('Reading {f}'.format(f=file.split(os.path.sep)[-2]))
+    with h5py.File(file) as d:
         t_points = d['data']['t_points'][:]
-        T = t_points.max()
-        tStart = T / 2
-        dt = np.diff(d['data']['t_points'][:])[0]
-        epr = d['data']['epr'][()]
-        epr_blind = d['data']['epr_blind'][()]
-        # epr_spectral = d['data']['epr_spectral'][()]
-        epr_spectral = [(fe.entropy(traj, sample_spacing=dt, sigma=5)).real for traj in d['data']['trajs'][..., t_points > tStart]]
-    return [epr, epr_blind, np.mean(epr_spectral), np.std(epr_spectral)]
+        t_epr = np.where(t_points > 1000)[0]  # only calculate epr after t = 10
+        dt = np.diff(t_points)[0]
+        nSim = d['params']['nSim'][()]
+        s = np.zeros(nSim)
+        rhos = np.zeros((nSim, len(t_epr)))
+
+        for ind, traj in enumerate(d['data']['trajs'][..., t_epr]):
+            s[ind], rhos[ind], w = fe.entropy(traj, sample_spacing=dt,
+                                              sigma=sigma, return_density=True)
+
+        # if '/data/s' in d:
+        #     d['data']['s'][...] = s
+        # else:
+        #     d['data'].create_dataset('s', data=s)
+
+        # if '/data/s_density' in d:
+        #     d['data']['s_density'][...] = rhos
+        # else:
+        #     d['data'].create_dataset('s_density', data=rhos)
+
+        # if '/data/omega' in d:
+        #     d['data']['omega'][...] = w
+        # else:
+        #     d['data'].create_dataset('omega', data=w)
+
+        # if '/params/sigma' in d:
+        #     d['params']['sigma'][...] = sigma
+        # else:
+        #     d['params'].create_dataset('sigma', data=sigma)
+    return s, rhos, w
 
 
-folder = '/mnt/llmStorage203/Danny/brusselatorSims/reactionsOnly/190313/blindBruss'
+if sys.platform == 'linux':
+    folder = '/mnt/llmStorage203/Danny/brusselatorSims/reactionsOnly/190904/'
+if sys.platform == 'darwin':
+    folder = '/Volumes/Storage/Danny/brusselatorSims/reactionsOnly/190904/'
 
-alpha = np.array([float(x.split(os.path.sep)[-1].split('_')[0].split('alpha')[-1]) for x in glob(os.path.join(folder, '*.hdf5'))])
+files = glob(os.path.join(folder, 'alpha*', 'data.hdf5'))
+sigma = 50
 
-# epr = np.zeros(len(alpha))
-
-# sigmaArray = [10]  # list(range(1, 11))
-# epr_spectral = np.zeros((len(sigmaArray), len(alpha)))
-# # epr_spectral = np.zeros(len(alpha))
-
-files = glob(os.path.join(folder, '*.hdf5'))
-
-print('Getting and calculating eprs...')
-with multiprocessing.Pool(processes=8) as pool:
+print('Calculating eprs...')
+with multiprocessing.Pool(processes=4) as pool:
     result = pool.map(calc_epr_spectral, files)
 print('Done.')
-
-# fig, ax = plt.subplots()
-# ax.loglog(alpha, epr, '.', label='True EPR')
-# ax.loglog(alpha, epr_spectral.T, '.', label='Spectral EPR')
-# ax.set(xlabel=r'$\alpha$', ylabel=r'$\dot{\Sigma}$')
-# plt.legend()
-
-# plt.show()
--- freqent/tests/brussfield/make_eprPlot.py
+++ freqent/tests/brussfield/make_eprPlot.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,19 +6,23 @@ import h5py
 import matplotlib as mpl
 mpl.rcParams['pdf.fonttype'] = 42
 
-parentFolder = '/mnt/llmStorage203/Danny/brusselatorSims/fieldSims/190509/brussfield'
+if sys.platform == 'linux':
+    parentFolder = '/mnt/llmStorage203/Danny/brusselatorSims/fieldSims/190905/'
+elif sys.platform == 'darwin':
+    parentFolder = '/Volumes/Storage/Danny/brusselatorSims/fieldSims/190905/'
+
 folders = glob(os.path.join(parentFolder, 'alpha*'))
 alphas = np.asarray([float(a.split(os.path.sep)[-1].split('_')[0][5:]) for a in folders])
 
-epr = np.zeros(len(alphas))
+epr_spectral = np.zeros((len(alphas), 10))
 epr_blind = np.zeros(len(alphas))
-epr_spectral = np.zeros(len(alphas))
+epr = np.zeros(len(alphas))
 
 for fInd, f in enumerate(folders):
     with h5py.File(os.path.join(f, 'data.hdf5'), 'r') as d:
         epr[fInd] = d['data']['epr'][()]
         epr_blind[fInd] = d['data']['epr_blind'][()]
-        epr_spectral[fInd] = d['data']['epr_spectral'][()]
+        epr_spectral[fInd] = d['data']['s'][:]
 
         if fInd == 0:
             lCompartment = d['params']['lCompartment'][()]
@@ -27,13 +31,14 @@ for fInd, f in enumerate(folders):
 V = lCompartment * nCompartments
 
 fig, ax = plt.subplots()
-ax.plot(alphas, epr / V, 'o', label='epr')
-ax.plot(alphas, epr_blind / V, 'o', label='epr_blind')
-ax.plot(alphas, epr_spectral, 'o', label='epr_spectral')
+ax.plot(np.log(alphas), epr / V, '.', label='epr')
+ax.plot(np.log(alphas), epr_blind / V, '.', label='epr_blind')
+# ax.errorbar(alphas, np.mean(epr_spectral, axis=1), yerr=np.std(epr_spectral, axis=1), fmt='ko', label='epr_spectral')
+ax.plot(np.log(np.repeat(np.sort(alphas), 10)), np.ravel(epr_spectral[np.argsort(alphas), :]), 'k.')
 
 ax.set(xlabel=r'$\alpha$', ylabel=r'$\dot{\Sigma}$')
-ax.set_aspect(np.diff(ax.get_xlim())[0] / np.diff(ax.get_ylim())[0])
-ax.set(yscale='log', xscale='log')
+# ax.set_aspect(np.diff(ax.get_xlim())[0] / np.diff(ax.get_ylim())[0])
+ax.set(yscale='log')
 plt.legend()
 
 plt.tight_layout()
