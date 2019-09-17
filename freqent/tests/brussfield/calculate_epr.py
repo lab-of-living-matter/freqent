92eb53617dac82e7f7edfc2311b958ec8a1efd4f
Danny Seara <dsseara@gmail.com>
2019-09-17 13:39:40 -0400

    changed params in brussfield/calculate_epr and make_eprPlot

--- freqent/tests/brussfield/calculate_epr.py
+++ freqent/tests/brussfield/calculate_epr.py
@@ -17,7 +17,7 @@ def calc_epr_spectral(file):
         t_epr = np.where(t_points > 10)[0]
         dt = np.diff(t_points)[0]
         dx = d['params']['lCompartment'][()]
-        nCompartments = d['params']['nCompartments'][()]
+        # nCompartments = d['params']['nCompartments'][()]
         nSim = d['params']['nSim'][()]
 
         s = np.zeros(nSim)
@@ -28,46 +28,43 @@ def calc_epr_spectral(file):
             s[ind], rhos[ind], w = fen.entropy(traj, sample_spacing=[dt, dx],
                                                window='boxcar', detrend='constant',
                                                smooth_corr=True, nfft=None,
-                                               sigma=int(d['params']['sigma'][()]),
+                                               sigma=sigma,
                                                subtract_bias=True,
                                                many_traj=False,
                                                return_density=True)
 
         if '/data/s' in d:
-            del d['data']['s'][...]
-            d['data'].create_dataset('s', data='s')
-        else:
-            d['data'].create_dataset('s', data='s')
+            del d['data']['s']
+        d['data'].create_dataset('s', data=s)
 
         if '/data/rhos' in d:
-            del d['data']['rhos'][...]
-            d['data'].create_dataset('rhos', data=rhos)
-        else:
-            d['data'].create_dataset('rhos', data=rhos)
+            del d['data']['rhos']
+        d['data'].create_dataset('rhos', data=rhos)
 
         if '/data/omega' in d:
-            del d['data']['omega'][...]
-            d['data'].create_dataset('omega', data=w[0])
-        else:
-            d['data'].create_dataset('omega', data=w[0])
+            del d['data']['omega']
+        d['data'].create_dataset('omega', data=w[0])
 
         if '/data/k' in d:
-            del d['data']['k'][...]
-            d['data'].create_dataset('k', data=w[1])
-        else:
-            d['data'].create_dataset('k', data=w[1])
+            del d['data']['k']
+        d['data'].create_dataset('k', data=w[1])
+
+        if '/params/sigma/' in d:
+            del d['params']['sigma']
+        d['params'].create_dataset('sigma', data=sigma)
 
     return s, rhos, w
 
 
 if sys.platform == 'darwin':
-    dataFolder = '/Volumes/Storage/Danny/brusselatorSims/fieldSims/190905'
+    dataFolder = '/Volumes/Storage/Danny/brusselatorSims/fieldSims/190910'
 if sys.platform == 'linux':
-    dataFolder = '/mnt/llmStorage203/Danny/brusselatorSims/fieldSims/190905'
+    dataFolder = '/mnt/llmStorage203/Danny/brusselatorSims/fieldSims/190910'
 
 files = glob(os.path.join(dataFolder, 'alpha*', 'data.hdf5'))
+sigma = [100, 5]
 
 print('Calculating eprs...')
-with multiprocessing.Pool(processes=2) as pool:
+with multiprocessing.Pool(processes=4) as pool:
     result = pool.map(calc_epr_spectral, files)
 print('Done.')
--- freqent/tests/brussfield/make_eprPlot.py
+++ freqent/tests/brussfield/make_eprPlot.py
@@ -1,15 +1,27 @@
-import os, sys
+import os
+import sys
 from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
+from datetime import datetime
 import matplotlib as mpl
+
+plt.close('all')
 mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['font.size'] = 12
+mpl.rcParams['axes.linewidth'] = 2
+mpl.rcParams['xtick.major.width'] = 2
+mpl.rcParams['xtick.minor.width'] = 2
+mpl.rcParams['ytick.major.width'] = 2
+mpl.rcParams['ytick.minor.width'] = 2
 
 if sys.platform == 'linux':
-    parentFolder = '/mnt/llmStorage203/Danny/brusselatorSims/fieldSims/190905/'
+    parentFolder = '/mnt/llmStorage203/Danny/brusselatorSims/fieldSims/190910/'
+    saveFolder = '/media/daniel/storage11/Dropbox/LLM_Danny/freqent/brussfield/'
 elif sys.platform == 'darwin':
-    parentFolder = '/Volumes/Storage/Danny/brusselatorSims/fieldSims/190905/'
+    parentFolder = '/Volumes/Storage/Danny/brusselatorSims/fieldSims/190910/'
+    saveFolder = '/media/daniel/storage11/Dropbox/LLM_Danny/freqent/brussfield/'
 
 folders = glob(os.path.join(parentFolder, 'alpha*'))
 alphas = np.asarray([float(a.split(os.path.sep)[-1].split('_')[0][5:]) for a in folders])
@@ -27,21 +39,32 @@ for fInd, f in enumerate(folders):
         if fInd == 0:
             lCompartment = d['params']['lCompartment'][()]
             nCompartments = d['params']['nCompartments'][()]
+            T = d['data']['t_points'][:].max()
+            dw = np.diff(d['data']['omega'][:])[0]
+            dk = np.diff(d['data']['k'][:])[0]
+            k_max = d['data']['k'][:].max()
+            w_max = d['data']['omega'][:].max()
+            sigma = d['params']['sigma'][:]
 
 V = lCompartment * nCompartments
+nrep = 1
+nvar = 2
+sigma_w = sigma[0] * dw
+sigma_k = sigma[1] * dk
+bias = (1 / nrep) * (nvar * (nvar - 1) / 2) * (w_max / (T * sigma_w * np.sqrt(np.pi))) * (k_max / (V * sigma_k * np.sqrt(np.pi)))
 
-fig, ax = plt.subplots()
-ax.plot(np.log(alphas), epr / V, '.', label='epr')
-ax.plot(np.log(alphas), epr_blind / V, '.', label='epr_blind')
-# ax.errorbar(alphas, np.mean(epr_spectral, axis=1), yerr=np.std(epr_spectral, axis=1), fmt='ko', label='epr_spectral')
-ax.plot(np.log(np.repeat(np.sort(alphas), 10)), np.ravel(epr_spectral[np.argsort(alphas), :]), 'k.')
+fig, ax = plt.subplots(figsize=(5.5, 5))
+ax.plot(alphas, epr / V, 'o', label='epr')
+ax.plot(alphas, epr_blind / V, 'o', label='epr_blind')
+ax.errorbar(alphas, np.mean(epr_spectral, axis=1), yerr=np.std(epr_spectral, axis=1), fmt='ko', label='epr_spectral', capsize=5)
+# ax.plot(np.log(np.repeat(np.sort(alphas), 10)), np.ravel(epr_spectral[np.argsort(alphas), :]), 'k.')
 
-ax.set(xlabel=r'$\alpha$', ylabel=r'$\dot{\Sigma}$')
+ax.set(xlabel=r'$\alpha$', ylabel=r'$\dot{S}$', xlim=(0.027, 35))
 # ax.set_aspect(np.diff(ax.get_xlim())[0] / np.diff(ax.get_ylim())[0])
-ax.set(yscale='log')
-plt.legend()
-
+ax.tick_params(which='both', direction='in')
+ax.set(yscale='log', xscale='log')
+ax.legend()
 plt.tight_layout()
 
-# fig.savefig(os.path.join(parentFolder, 'eprPlot.pdf'), format='pdf')
+fig.savefig(os.path.join(saveFolder, datetime.now().strftime('%y%m%d') + '_eprPlot.pdf'), format='pdf')
 plt.show()
