a5d51024ab76e78c4b53cf3ada866b7a733121f3
Danny Seara <dsseara@gmail.com>
2019-09-17 18:52:21 -0400

    created script to make a plot showing the region fit using a line for brussealtor

---
+++ freqent/tests/brusselator/make_epTrajFitPlot.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import matplotlib as mpl
+from datetime import datetime
+from scipy import stats
+
+# plt.close('all')
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['font.size'] = 12
+mpl.rcParams['axes.linewidth'] = 2
+mpl.rcParams['xtick.major.width'] = 2
+mpl.rcParams['xtick.minor.width'] = 2
+mpl.rcParams['ytick.major.width'] = 2
+mpl.rcParams['ytick.minor.width'] = 2
+
+if sys.platform == 'linux':
+    datapath = '/mnt/llmStorage203/Danny/brusselatorSims/reactionsOnly/190904/'
+    savepath = '/media/daniel/storage11/Dropbox/LLM_Danny/freqent/brusselator/stochastic_simulations/'
+elif sys.platform == 'darwin':
+    datapath = '/Volumes/Storage/Danny/brusselatorSims/reactionsOnly/190904/'
+    savepath = '/Users/Danny/Dropbox/LLM_Danny/freqent/brusselator/stochastic_simulations/'
+
+file = 'alpha5.96_nSim50'
+
+with h5py.File(os.path.join(datapath, file, 'data.hdf5')) as d:
+    ep_blind_traj = d['data']['ep_blinds'][:, ::10]
+    epr_blind = d['data']['epr_blind'][()]
+    t_points = d['data']['t_points'][::10]
+
+r = stats.linregress(t_points[len(t_points) // 2:],
+                     ep_blind_traj[:, len(t_points) // 2:].mean(axis=0))
+
+fig, ax = plt.subplots()
+ax.plot(t_points, ep_blind_traj.T, 'k', alpha=0.1, lw=0.5)
+ax.plot(t_points, ep_blind_traj.mean(axis=0), 'k')
+ax.plot(t_points[len(t_points) // 2:],
+        t_points[len(t_points) // 2:] * r.slope + r.intercept,
+        'r--', lw=2)
+
+ax.tick_params(which='both', direction='in')
+ax.set(xlabel='t', ylabel=r'$\Delta S_{blind}$', title=r'$\alpha = 5.96$')
+ax.set_aspect(np.diff(ax.get_xlim())[0] / np.diff(ax.get_ylim())[0])
+
+legend_handles = [mpl.lines.Line2D([0], [0], color='k', alpha=0.1, lw=0.5, label='trajectories'),
+                  mpl.lines.Line2D([0], [0], color='k', label='mean'),
+                  mpl.lines.Line2D([0], [0], linestyle='--', color='r', lw=2, label='linear fit')]
+ax.legend(handles=legend_handles, loc='best')
+fig.savefig(os.path.join(savepath, datetime.now().strftime('%y%m%d') + '_ep_blind_traj_alpha5.96.pdf'), format='pdf')
+fig.savefig(os.path.join(savepath, datetime.now().strftime('%y%m%d') + '_ep_blind_traj_alpha5.96.png'), format='png')
+
+plt.show()
