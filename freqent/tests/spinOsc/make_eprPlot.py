a784287bc10b1da31f48e243e3374d10434a782d
Danny Seara <dsseara@gmail.com>
2019-10-06 19:49:46 -0400

    changed some plotting things i guess

--- freqent/tests/spinOsc/make_eprPlot.py
+++ freqent/tests/spinOsc/make_eprPlot.py
@@ -15,18 +15,18 @@ mpl.rcParams['xtick.major.width'] = 2
 mpl.rcParams['ytick.major.width'] = 2
 
 if sys.platform == 'linux':
-    dataPath = '/mnt/llmStorage203/Danny/freqent/spinOsc/190709/'
-    savePath = '/media/daniel/storage11/Dropbox/LLM_Danny/freqent/spinOsc/'
+    datapath = '/mnt/llmStorage203/Danny/freqent/spinOsc/190709/'
+    savepath = '/media/daniel/storage11/Dropbox/LLM_Danny/freqent/spinOsc/'
 elif sys.platform == 'darwin':
-    dataPath = '/Volumes/Storage/Danny/freqent/spinOsc/190709/'
-    savePath = '/Users/Danny/Dropbox/LLM_Danny/freqent/spinOsc/'
+    datapath = '/Volumes/Storage/Danny/freqent/spinOsc/190709/'
+    savepath = '/Users/Danny/Dropbox/LLM_Danny/freqent/spinOsc/'
 
 alphas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ndims = [2, 3, 4]
 nsim = 64
 s_array = np.zeros((len(ndims), len(alphas), nsim))
 
-for file in glob(os.path.join(dataPath, '*.hdf5')):
+for file in glob(os.path.join(datapath, '*.hdf5')):
     with h5py.File(file, 'r') as d:
         dim = d['params']['ndim'][()]
         alpha = d['params']['alpha'][()]
@@ -43,7 +43,7 @@ ax2.set(xlabel=r'$\alpha$', ylabel=r'$\dot{S}$', title='2 dimensions', ylim=[-14
 ax2.set_aspect(np.diff(ax2.set_xlim())[0] / np.diff(ax2.set_ylim())[0])
 ax2.legend()
 plt.tight_layout()
-fig2.savefig(os.path.join(savePath, datetime.now().strftime('%y%m%d') + '_eprPlot_2dim.pdf'), format='pdf')
+fig2.savefig(os.path.join(savepath, datetime.now().strftime('%y%m%d') + '_eprPlot_2dim.pdf'), format='pdf')
 
 
 fig3, ax3 = plt.subplots(figsize=(5, 5))
@@ -57,7 +57,7 @@ ax3.set(xlabel=r'$\alpha$', ylabel=r'$\dot{S}$', title='3 dimensions', ylim=[-14
 ax3.set_aspect(np.diff(ax3.set_xlim())[0] / np.diff(ax3.set_ylim())[0])
 ax3.legend()
 plt.tight_layout()
-fig3.savefig(os.path.join(savePath, datetime.now().strftime('%y%m%d') + '_eprPlot_3dim.pdf'), format='pdf')
+fig3.savefig(os.path.join(savepath, datetime.now().strftime('%y%m%d') + '_eprPlot_3dim.pdf'), format='pdf')
 
 fig4, ax4 = plt.subplots(figsize=(5, 5))
 ax4.errorbar(alphas, np.mean(s_array[2], axis=1), yerr=np.std(s_array[2], axis=1),
@@ -70,7 +70,7 @@ ax4.set(xlabel=r'$\alpha$', ylabel=r'$\dot{S}$', title='4 dimensions', ylim=[-14
 ax4.set_aspect(np.diff(ax4.set_xlim())[0] / np.diff(ax4.set_ylim())[0])
 ax4.legend()
 plt.tight_layout()
-fig4.savefig(os.path.join(savePath, datetime.now().strftime('%y%m%d') + '_eprPlot_4dim.pdf'), format='pdf')
+fig4.savefig(os.path.join(savepath, datetime.now().strftime('%y%m%d') + '_eprPlot_4dim.pdf'), format='pdf')
 
 figall, axall = plt.subplots(figsize=(5, 5))
 axall.errorbar(alphas, np.mean(s_array[0], axis=1), yerr=np.std(s_array[2], axis=1),
@@ -89,6 +89,6 @@ axall.set(xlabel=r'$\alpha$', ylabel=r'$\dot{S}$', title='2,3,4 dimensions', yli
 axall.set_aspect(np.diff(axall.set_xlim())[0] / np.diff(axall.set_ylim())[0])
 axall.legend()
 plt.tight_layout()
-figall.savefig(os.path.join(savePath, datetime.now().strftime('%y%m%d') + '_eprPlot_alldim.pdf'), format='pdf')
+figall.savefig(os.path.join(savepath, datetime.now().strftime('%y%m%d') + '_eprPlot_alldim.pdf'), format='pdf')
 
 plt.show()
