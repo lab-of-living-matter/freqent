620d50e74a0bf12eab816beb36f244ca563d699c
Danny Seara <dsseara@gmail.com>
2019-10-10 13:43:08 -0400

    now go through all files, plot phase flux, and save the plot

--- freqent/tests/brusselator/make_phaseFluxPlot.py
+++ freqent/tests/brusselator/make_phaseFluxPlot.py
@@ -3,38 +3,44 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import h5py
 import argparse
-from probabilityFlux import *
+import os
+from glob import glob
 
 plt.close('all')
 mpl.rcParams['pdf.fonttype'] = 42
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--filepath', '-file', type=str,
-                    help='absolute path to hdf5 file with data')
-parser.add_argument('--savepath', '-save', type=str,
-                    help='absolute path to save plot')
+parser.add_argument('--datapath', '-file', type=str,
+                    help='absolute path to parent folder of all alpha* folders with data')
+parser.add_argument('--savepath', type=str)
 
 args = parser.parse_args()
+files = glob(os.path.join(args.datapath, 'alpha*', 'data.hdf5'))
 
-with h5py.File(args.filepath) as d:
-    edges_x = d['data']['edges_x'][:]
-    edges_y = d['data']['edges_y'][:]
-    flux_field = d['data']['flux_field'][:]
-    prob_map = d['data']['prob_map'][:]
-    alpha = d['params']['B'][()] * d['params']['rates'][2] * d['params']['rates'][4] / (d['params']['C'][()] * d['params']['rates'][3] * d['params']['rates'][5])
+for file in files:
+    print(file.split(os.path.sep)[-2])
+    with h5py.File(file) as d:
+        edges_x = d['data']['edges_x'][:]
+        edges_y = d['data']['edges_y'][:]
+        flux_field = d['data']['flux_field'][:]
+        prob_map = d['data']['prob_map'][:]
+        alpha = (d['params']['B'][()] * d['params']['rates'][2] * d['params']['rates'][4] /
+                 (d['params']['C'][()] * d['params']['rates'][3] * d['params']['rates'][5]))
 
-xx, yy = np.meshgrid(edges_x[:-1], edges_y[:-1])
-dx, dy = [np.diff(edges_x)[0], np.diff(edges_y)[0]]
-yrange = edges_y[-1] - edges_y[0]
+    xx, yy = np.meshgrid(edges_x[:-1], edges_y[:-1])
+    dx, dy = [np.diff(edges_x)[0], np.diff(edges_y)[0]]
+    yrange = edges_y[-1] - edges_y[0]
 
-fig, ax = plt.subplots(figsize=(5, 5))
-ax.pcolormesh(edges_x, edges_y, prob_map.T, rasterized=True)
-ax.quiver(xx[::2, ::2] + dx / 2, yy[::2, ::2] + dy / 2, flux_field[0][::2, ::2].T, flux_field[1][::2, ::2].T,
-          color='w', scale=1)
+    plt.close('all')
+    fig, ax = plt.subplots(figsize=(7, 10))
+    ax.pcolormesh(edges_x, edges_y, prob_map.T, rasterized=True, cmap='Blues')
+    ax.quiver(xx[::2, ::2] + dx / 2, yy[::2, ::2] + dy / 2, flux_field[0][::2, ::2].T, flux_field[1][::2, ::2].T,
+              color='k', alpha=0.5, scale=1)
 
-ax.set_aspect('equal')
-ax.set(xlabel='X', ylabel='Y', title=r'$\alpha = {a}$'.format(a=alpha))
-ax.tick_params(which='both', direction='in')
-plt.tight_layout()
+    ax.set_aspect('equal')
+    ax.set(xlabel='X', ylabel='Y', title=r'$\alpha = {a}$'.format(a=alpha), xlim=[0, 800], ylim=[0, 1100])
+    ax.tick_params(which='both', direction='in')
+    plt.tight_layout()
 
-plt.show()
+    fig.savefig(os.path.join(args.savepath, 'phaseFluxPlot_alpha{a}.png'.format(a=alpha)), format='png')
+
--- freqent/tests/brusselator/make_vSquaredAlphaPlot.py
+++ freqent/tests/brusselator/make_vSquaredAlphaPlot.py
@@ -1,9 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import h5py
 import sys
 import os
 from glob import glob
+from datetime import datetime
+
+plt.close('all')
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['font.size'] = 12
+mpl.rcParams['axes.linewidth'] = 1
+mpl.rcParams['xtick.major.width'] = 1
+mpl.rcParams['xtick.minor.width'] = 1
+mpl.rcParams['ytick.major.width'] = 1
+mpl.rcParams['ytick.minor.width'] = 1
+
 
 if sys.platform == 'linux':
     datapath = '/mnt/llmStorage203/Danny/brusselatorSims/reactionsOnly/190904/'
@@ -16,6 +28,10 @@ files = glob(os.path.join(datapath, 'alpha*', 'data.hdf5'))
 
 alphas = np.zeros(len(files))
 vsqs = np.zeros(len(files))
+epr_blind = np.zeros(len(files))
+epr = np.zeros(len(files))
+epr_spectral = np.zeros((len(alphas), 50))
+
 for ind, file in enumerate(files):
     with h5py.File(file) as d:
         alphas[ind] = (d['params']['B'][()] * d['params']['rates'][2] * d['params']['rates'][4] /
@@ -23,8 +39,25 @@ for ind, file in enumerate(files):
         dx, dy = np.diff(d['data']['edges_x'])[0], np.diff(d['data']['edges_y'])[0]
         dudy, dudx = np.gradient(d['data']['flux_field'][0].T, 4)
         dvdy, dvdx = np.gradient(d['data']['flux_field'][1].T, 4)
-        vsqs[ind] = np.sum(d['data']['flux_field'][0].T**2 + d['data']['flux_field'][1].T**2)
+        vsqs[ind] = np.sum(d['data']['flux_field'][0].T**2 + d['data']['flux_field'][1].T**2) / (dx * dy)
+        epr[ind] = d['data']['epr'][()]
+        epr_blind[ind] = d['data']['epr_blind'][()]
+        epr_spectral[ind] = d['data']['s'][:]
 
+fig, ax = plt.subplots(figsize=(5.5, 5))
+ax.loglog(alphas, epr, 'o', label=r'$\dot{S}_{true}$')
+ax.loglog(alphas, epr_blind, 'o', label=r'$\dot{S}_{blind}$')
+ax.loglog(alphas, np.mean(epr_spectral, axis=1), 'ko', label=r'$\hat{\dot{S}}$')
+ax.fill_between(alphas[np.argsort(alphas)],
+                np.mean(epr_spectral, axis=1)[np.argsort(alphas)] + np.std(epr_spectral, axis=1)[np.argsort(alphas)],
+                np.mean(epr_spectral, axis=1)[np.argsort(alphas)] - np.std(epr_spectral, axis=1)[np.argsort(alphas)],
+                color='k', alpha=0.5)
+ax.loglog(alphas, vsqs, 'o', color='C3', label=r'$\dot{S}_{\int v^2}$', alpha=0.7)
+ax.set(xlabel=r'$\alpha$', ylabel=r'$\dot{S}$')
+ax.tick_params(which='both', direction='in')
+ax.legend(loc='lower right')
+plt.tight_layout()
 
-fig, ax = plt.subplots()
-ax.plot(alphas, vsqs, 'ko')
+fig.savefig(os.path.join(savepath, datetime.now().strftime('%y%m%d') + '_vSquared_vs_alpha.pdf'), format='pdf')
+
+plt.show()
