16eead7ada35002f05c11bcda2848d927caeef19
Danny Seara <dsseara@gmail.com>
2019-10-28 15:16:06 -0400

    brusselator epr plots mu instead of alpha

--- freqent/tests/brusselator/make_eprPlot.py
+++ freqent/tests/brusselator/make_eprPlot.py
@@ -37,24 +37,24 @@ for fInd, f in enumerate(folders):
         epr_spectral[fInd] = d['data']['s'][:]
 
 fig, ax = plt.subplots(figsize=(5.5, 5))
-ax.plot(alphas, epr, 'o', label=r'$\dot{S}_{true}$')
-ax.plot(alphas, epr_blind, 'o', label=r'$\dot{S}_{blind}$')
+ax.plot(np.log(alphas), epr, 'o', label=r'$\dot{S}_{true}$')
+ax.plot(np.log(alphas), epr_blind, 'o', label=r'$\dot{S}_{blind}$')
 # ax.errorbar(alphas, np.mean(epr_spectral, axis=1),
 #             yerr=np.std(epr_spectral, axis=1), fmt='ko',
 #             label=r'$\dot{S}_{spectral}$', capsize=5)
-ax.plot(alphas, np.mean(epr_spectral, axis=1), 'ko', label=r'$\dot{S}_{spectral}$')
-ax.fill_between(alphas[np.argsort(alphas)],
+ax.plot(np.log(alphas), np.mean(epr_spectral, axis=1), 'ko', label=r'$\dot{S}_{spectral}$')
+ax.fill_between(np.log(alphas)[np.argsort(alphas)],
                 np.mean(epr_spectral, axis=1)[np.argsort(alphas)] + np.std(epr_spectral, axis=1)[np.argsort(alphas)],
                 np.mean(epr_spectral, axis=1)[np.argsort(alphas)] - np.std(epr_spectral, axis=1)[np.argsort(alphas)],
                 color='k', alpha=0.5)
 
 # ax.plot(np.repeat(np.sort(alphas), 50), np.ravel(epr_spectral[np.argsort(alphas), :]), 'k.', alpha=0.5)
 
-ax.set(xlabel=r'$\alpha$', ylabel=r'$\dot{S}$')
+ax.set(xlabel=r'$\Delta \mu$', ylabel=r'$\dot{S}$')
 # ax.set_aspect(np.diff(ax.get_xlim())[0] / np.diff(ax.get_ylim())[0])
 ax.set(yscale='log', xscale='log')
 ax.tick_params(which='both', direction='in')
-ax.legend(loc='lower right')
+ax.legend(loc='best')
 plt.tight_layout()
 
 # fig.savefig(os.path.join(saveFolder, datetime.now().strftime('%y%m%d') + '_eprPlot.pdf'), format='pdf')
