3355012b0dcd97fd91bf466f215367ccb1b2f16d
Danny Seara <dsseara@gmail.com>
2019-11-22 18:12:59 -0500

    changed some plotting parameters for deterministic brusselator

--- freqent/tests/brusselator/brusselator_deterministicSim.py
+++ freqent/tests/brusselator/brusselator_deterministicSim.py
@@ -17,8 +17,17 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+
+
+plt.close('all')
 mpl.rcParams['pdf.fonttype'] = 42
-# plt.close('all')
+mpl.rcParams['pdf.fonttype'] = 42
+mpl.rcParams['font.size'] = 12
+mpl.rcParams['axes.linewidth'] = 2
+mpl.rcParams['xtick.major.width'] = 2
+mpl.rcParams['xtick.minor.width'] = 2
+mpl.rcParams['ytick.major.width'] = 2
+mpl.rcParams['ytick.minor.width'] = 2
 
 t = np.linspace(0, 100, 2001)
 a = 1
@@ -59,10 +68,11 @@ for r0 in np.random.rand(100, 2) * 1.5 * bc:
 
 legend_elements = [mpl.lines.Line2D([0], [0], markersize=10, marker='X',
                                     linestyle='None', markeredgecolor='k',
-                                    color='r', label=r'fixed point, $(a, b/a)$'),
+                                    color='r', label=r'fixed point'),
                    mpl.lines.Line2D([0], [0], color='k', label='trajectory')]
 
 ax.legend(handles=legend_elements, loc='best')
-ax.set(xlabel=r'$[X]$', ylabel=r'$[Y]$', title=r'Brusselator, $a$={a}, $b_c$={bc}, $b=${b}'.format(a=a, bc=bc, b=b))
-
+ax.set(xlabel=r'$[X]$', ylabel=r'$[Y]$')#, title=r'Brusselator, $a$={a}, $b_c$={bc}, $b=${b}'.format(a=a, bc=bc, b=b))
+ax.set_aspect(np.diff(ax.set_xlim())[0] / np.diff(ax.set_ylim())[0])
+ax.tick_params(which='both', direction='in')
 plt.show()
