61c64a2d6ddf9562fdd75818719eefdbb05beba1
Danny Seara <dsseara@gmail.com>
2019-11-22 17:05:36 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='freqent',
-      version='0.0.3',
+      version='0.1.0',
       description='Calculating entropy in frequency space',
       url='http://gitlab.com/dsseara/freqent',
       author='Daniel S. Seara',
