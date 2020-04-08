e9ca2af3040cb50b0bc36fd58be985ccb9df84c4
Danny Seara <dsseara@gmail.com>
2020-04-08 15:24:40 -0400

    updated make_exampleTraj to use newest simulations

--- freqent/tests/brusselator/make_exampleTraj.py
+++ freqent/tests/brusselator/make_exampleTraj.py
@@ -17,13 +17,13 @@ mpl.rcParams['ytick.major.width'] = 2
 mpl.rcParams['ytick.minor.width'] = 2
 
 if sys.platform == 'linux':
-    datapath = '/mnt/llmStorage203/Danny/brusselatorSims/reactionsOnly/190904/'
+    datapath = '/mnt/llmStorage203/Danny/brusselatorSims/reactionsOnly/191026/'
     savepath = '/media/daniel/storage11/Dropbox/LLM_Danny/freqent/brusselator/stochastic_simulations'
 elif sys.platform == 'darwin':
-    datapath = '/Volumes/Storage/Danny/brusselatorSims/reactionsOnly/190904/'
+    datapath = '/Volumes/Storage/Danny/brusselatorSims/reactionsOnly/191026/'
     savepath = '/Users/Danny/Dropbox/LLM_Danny/freqent/brusselator/stochastic_simulations'
 
-datafile = os.path.join(datapath, 'alpha55.0_nSim50', 'data.hdf5')
+datafile = os.path.join(datapath, 'alpha200.33680997479286_nSim50', 'data.hdf5')
 
 
 with h5py.File(datafile) as d:
@@ -38,16 +38,16 @@ with h5py.File(datafile) as d:
 prob = prob_map / prob_map.sum()
 
 cmap_prob = mpl.cm.get_cmap('Blues')
-normalize_prob = mpl.colors.Normalize(vmin=prob.min(), vmax=prob.max())
+normalize_prob = mpl.colors.Normalize(vmin=np.log(prob).min(), vmax=np.log(prob).max())
 
 xx, yy = np.meshgrid(edges_x[:-1], edges_y[:-1])
 dx, dy = [np.diff(edges_x)[0], np.diff(edges_y)[0]]
 yrange = edges_y[-1] - edges_y[0]
 
-inds = np.logical_and(t > 2004, t < 2006)
+inds = np.logical_and(t > 800, t < 806.5)
 
 fig, ax = plt.subplots(figsize=(5, 5))
-ax.pcolormesh(edges_x, edges_y, prob.T, rasterized=True, cmap='Blues')
+ax.pcolormesh(edges_x, edges_y, np.log(prob.T), rasterized=True, cmap='Blues')
 ax.plot(traj[0, inds], traj[1, inds], 'k', alpha=0.7)
 ax.plot(traj[0, np.where(inds)[0][-1]], traj[1, np.where(inds)[0][-1]],
         'o', markersize=15, markeredgecolor='k', color=(0.9, 0.9, 0.9))
@@ -57,7 +57,7 @@ cbar2 = mpl.colorbar.ColorbarBase(cax2, cmap=cmap_prob, norm=normalize_prob)
 cbar2.ax.set_title(r'$p(X, Y)$')
 
 ax.set_aspect('equal')
-ax.set(xlabel='X', ylabel='Y', title=r'$\alpha = {a}$'.format(a=alpha))
+ax.set(xlabel='X', ylabel='Y', title=r'$\mu = {m:0.2f}$'.format(m=np.log(alpha)))
 ax.tick_params(which='both', direction='in')
 
 
