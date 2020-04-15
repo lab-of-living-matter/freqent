d86c51de3fb5623232d67c1a7848b71a4c4ed0ca
Danny Seara <dsseara@gmail.com>
2020-04-15 10:11:21 -0400

    renamed brusselator_deterministicSim to make_deterministicSim and changed dynamics

--- freqent/tests/brusselator/make_deterministicSim.py
+++ freqent/tests/brusselator/make_deterministicSim.py
@@ -1,11 +1,12 @@
 '''
 Non-dimensionalized Brusselator model:
 
-dx/dt = a - (1 + b) * x + x^2 * y
-dy/dt = b * x - x^2 * y
+dx/dt = k1plus * a - k1minus * x + k2minus * y - k2plus * b * x + k3plus * x**2 * y - k3minus * x**3
+dy/dt =  -k2minus * y + k2plus * b * x - k3plus * x**2 * y + k3minus * x**3
 
-a,b > 0 are parameters
-x,y > 0 are non-dimensionalized concentrations
+k1plus, k1minus, k2plus, k2minus, k3plus, k3minus > 0 are parameters
+a, b, c > 0 are chemostat concentrations, (b,c) are determined by thermodynamic driving
+x, y > 0 are non-dimensionalized concentrations
 
 Fixed point at (a, b/a)
 Critical point at bc = 1 + a^2
@@ -17,7 +18,7 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
+import argparse
 
 plt.close('all')
 mpl.rcParams['pdf.fonttype'] = 42
@@ -29,31 +30,36 @@ mpl.rcParams['xtick.minor.width'] = 2
 mpl.rcParams['ytick.major.width'] = 2
 mpl.rcParams['ytick.minor.width'] = 2
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--deltaMu', '-mu', type=float, default=0.0,
+                    help='driving force')
+args = parser.parse_args()
+
+mu = args.deltaMu
 t = np.linspace(0, 100, 2001)
+k1plus, k1minus, k2plus, k2minus, k3plus, k3minus = [1, 0.5, 2, 0.5, 2, 0.5]
 a = 1
-bc = 1 + a**2
+b = np.sqrt(k2minus * k3minus / (k2plus * k3plus)) * np.exp(mu / 2)
+c = (k2minus * k3minus) / (b * k2plus * k3plus)
+xss = a * k1plus / k1minus
+yss = (k2plus * b * xss + k3minus * xss**3) / (k2minus * c + k3plus * xss**2)
 
-
-def brusselator(r, t, a, b):
+def brusselator(r, t, a, b, c, rates):
     '''
     r = [x, y]
     '''
     x, y = r
-    drdt = [a - (1 + b) * x + x**2 * y, b * x - x**2 * y]
+    k1plus, k1minus, k2plus, k2minus, k3plus, k3minus = rates
+    drdt = [k1plus * a - k1minus * x + k2minus * y - k2plus * b * x + k3plus * x**2 * y - k3minus * x**3,
+            -k2minus * y + k2plus * b * x - k3plus * x**2 * y + k3minus * x**3]
     return drdt
-
-
-b = bc * 0.5
-# cmap = mpl.cm.get_cmap('tab10')
-# normalize = mpl.colors.Normalize(vmin=min(bArray), vmax=max(bArray))
-# colors = [cmap(normalize(value)) for value in bArray]
 
 fig, ax = plt.subplots()
 
-for r0 in np.random.rand(100, 2) * 1.5 * bc:
-    sol = odeint(brusselator, r0, t, args=(a, b))
-    ax.plot(sol[:, 0], sol[:, 1], color='k', alpha=0.1)
-    ax.plot(a, b / a, marker='X', markersize=10, markeredgecolor='k', color='r')
+for r0 in np.random.rand(100, 2) * 1.5 * np.array(xss, yss):
+    sol = odeint(brusselator, r0, t, args=(a, b, c, [k1plus, k1minus, k2plus, k2minus, k3plus, k3minus]))
+    ax.plot(sol[len(t) // 2:, 0], sol[len(t) // 2:, 1], color='k', alpha=0.1)
+    ax.plot(xss, yss, marker='X', markersize=10, markeredgecolor='k', color='r')
 
 # for bInd, b in enumerate(bArray):
 #     r0 = np.random.rand(2) * bc
